ecbd45e3dedf1cd500c656b2e9e38a848eaac06f
Camille Renard <camille.renard@etu.umontpellier.fr>
2024-10-21 14:01:37 +0200

    connected database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env.MONGODB_URI);
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    // autres modules ici...
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
