b4c467cc146506892e4ec3e8b9d6d2788557f355
unknown <sarah.elmss@icloud.com>
2025-02-10 12:14:36 +0100

    change port to 5000

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@
 // main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 5000;
+  const PORT = process.env.PORT || 5000;
 
   // Activer CORS avec des configurations spécifiques
   app.enableCors({
@@ -15,5 +19,6 @@ async function bootstrap() {
   });
 
   await app.listen(PORT);
+  console.log(`Server is running on http://localhost:${PORT}`);
 }
 bootstrap();
