3e20550b90d869ef82ede5b16283065ad0b866bd
unknown <sarah.elmss@icloud.com>
2025-02-10 12:10:17 +0100

    change port to 5000

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = 5000;
 
   // Activer CORS avec des configurations spécifiques
   app.enableCors({
@@ -13,6 +14,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(5000);
+  await app.listen(PORT);
 }
 bootstrap();
